db0148ed08193aaefc6dbb2e62bf11fb51e70454
kenkomarinho <kenkomarinho@gmail.com>
2026-02-03 12:08:56 +0000

    Refactor URL routing to fix 404 errors

--- mysite/base/urls.py
+++ mysite/base/urls.py
@@ -4,10 +4,11 @@ from rest_framework import permissions, routers
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from drf_yasg.generators import OpenAPISchemaGenerator
+
+# Importando as views diretamente
 from mercadosorcery import views as mercadosorcery_views
 from socialentities import views as socialentities_views
-from django.conf import settings
-from django.conf.urls.static import static
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -44,15 +45,21 @@ router.register(r'listas', mercadosorcery_views.ListaViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # JWT Token Endpoints
+    # Rotas de documentação da API
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/?$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/?$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    # --- ROTAS DA API ---
+
+    # Rotas personalizadas
+    path('api/posses/adicionar/', mercadosorcery_views.AdicionarPosseView.as_view(), name='adicionar-posse'),
+    path('api/colecoes/minha-colecao/', mercadosorcery_views.MinhaColecaoView.as_view(), name='minha-colecao'),
+
+    # Autenticação JWT
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    # Incluindo as rotas padrão do router do DRF
+    # Rotas do DRF Router (deve ser a última rota da API)
     path('api/', include(router.urls)),
-
-    # Rotas da documentação da API
-    re_path(r'^swagger(?P<format>\\.json|\\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- mysite/mercadosorcery/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import AdicionarPosseView, MinhaColecaoView
-
-# Estas são as URLs que NÃO são gerenciadas pelo router do DRF
-urlpatterns = [
-    path('posses/adicionar/', AdicionarPosseView.as_view(), name='adicionar-posse'),
-    path('colecoes/minha-colecao/', MinhaColecaoView.as_view(), name='minha-colecao'),
-]
